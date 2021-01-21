2791b10e98bd309703e318b2ab05ad248791ad77
Atanes <alexandreatanes@parceirosbrk.com.br>
2021-01-21 09:53:48 -0300

    Inclusão de mais dados para internacionalização

--- src/app/features/Cavaletes/cavaletes.module.ts
+++ src/app/features/Cavaletes/cavaletes.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @NgModule({
   imports: [
     CommonModule,
+    TranslocoModule
   ]
 })
 export class CavaletesModule { }
